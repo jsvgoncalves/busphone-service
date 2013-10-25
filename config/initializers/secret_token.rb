4249f4f5fa47701c3f45aed8300607ae99ef5422
João Gonçalves <jsvgoncalves@gmail.com>
2013-10-25 15:14:05 +0100

    Added Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BusphoneService::Application.config.secret_key_base = '5df8b92790967fd36e997f1ac5533123b2b49a7af0a38b0ec9433896927cb63743e8834a6cb48a43e7fe588368c45783906f5b3aaf56086bb458fa56f1d3f372'
