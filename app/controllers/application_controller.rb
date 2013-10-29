1937bc01bc2eb073d11d94df55e6362d110014c1
João Gonçalves <jsvgoncalves@gmail.com>
2013-10-29 13:18:41 +0000

    Added record not found default rescue.
    It outputs a JSON saying a record was not found.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  def record_not_found
+  	# $! is the error 
+    render :json => 
+    	{ 
+	    	:error => {
+	    		:msg => 'record not found', :code => 2 }  
+    	}  
+    true
+  end
 end
